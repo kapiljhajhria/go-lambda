f9ab305032e14bdfc612895098f4a6cd68686dde
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-10-18 15:30:58 +0530

    print logs and update get method

--- handlers/helloWorld.go
+++ handlers/helloWorld.go
@@ -1,8 +1,18 @@
 package handlers
 
-import "github.com/aws/aws-lambda-go/events"
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+)
 
 func HelloWorldHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// TODO implement
+	if request.HTTPMethod == "GET" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body:       fmt.Sprintf("Hello World!"),
+		}, nil
+	}
+	fmt.Println("Hello Work-----------------------------------> called")
 	return events.APIGatewayProxyResponse{}, nil
 }
