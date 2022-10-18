1f18ff11b9b3bf785b17971915dbf1c87231a252
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-10-18 14:17:49 +0530

    go-lambda init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kapiljhajhria/go-lambda
+
+go 1.19
+
+require github.com/aws/aws-lambda-go v1.34.1
---
+++ handlers/helloWorld.go
@@ -0,0 +1,8 @@
+package handlers
+
+import "github.com/aws/aws-lambda-go/events"
+
+func HelloWorldHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// TODO implement
+	return events.APIGatewayProxyResponse{}, nil
+}
